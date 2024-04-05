4d8cab75489b7173bf0cc6e3021533466e5cd62f
soonhankwon <soonable@naver.com>
2024-04-05 19:16:44 +0900

    init

---
+++ src/main/java/dev/easylogbe/EasylogBeApplication.java
@@ -0,0 +1,13 @@
+package dev.easylogbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasylogBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasylogBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/easylogbe/EasylogBeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.easylogbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasylogBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
