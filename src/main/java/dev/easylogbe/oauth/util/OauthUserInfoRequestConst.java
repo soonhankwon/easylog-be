08c14202d3e8ba53c83e1a905d2766a5bf99e9a3
soonhankwon <soonable@naver.com>
2024-04-22 16:21:07 +0900

    feat: Oauth 유저정보 상수클래스(#3)

---
+++ src/main/java/dev/easylogbe/oauth/util/OauthUserInfoRequestConst.java
@@ -0,0 +1,12 @@
+package dev.easylogbe.oauth.util;
+
+public final class OauthUserInfoRequestConst {
+    public static final String ID = "id";
+    public static final String EMAIL = "email";
+    public static final String PROFILE = "profile";
+    public static final String KAKAO_ACCOUNT = "kakao_account";
+    public static final String KAKAO_NICKNAME = "nickname";
+    public static final String KAKAO_PROFILE_IMG_URL = "profile_image_url";
+    public static final String GOOGLE_PROFILE_IMG_URL = "picture";
+    public static final String GOOGLE_NICKNAME = "name";
+}
