1217ecd3f4e9468ac672b27244c6f2fe63ba669d
soonhankwon <soonable@naver.com>
2024-04-22 16:20:18 +0900

    feat: oauthToken DTO(#3)

---
+++ src/main/java/dev/easylogbe/oauth/dto/OauthTokenDTO.java
@@ -0,0 +1,21 @@
+package dev.easylogbe.oauth.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record OauthTokenDTO(
+        @JsonProperty(value = "token_type")
+        String tokeType,
+
+        @JsonProperty(value = "access_token")
+        String accessToken,
+
+        @JsonProperty(value = "expires_in")
+        Integer expiresIn,
+
+        @JsonProperty(value = "refresh_token")
+        String refreshToken,
+
+        @JsonProperty(value = "refresh_token_expires_in")
+        Integer refreshTokenExpiresIn
+) {
+}
