25aa154e3d3c7c44be08c145a3a3be78a741909e
soonhankwon <soonable@naver.com>
2024-04-22 16:21:47 +0900

    feat: findByEmail 추가(#3)

--- src/main/java/dev/easylogbe/user/repository/UserRepository.java
+++ src/main/java/dev/easylogbe/user/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package dev.easylogbe.user.repository;
 
 import dev.easylogbe.user.domain.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
